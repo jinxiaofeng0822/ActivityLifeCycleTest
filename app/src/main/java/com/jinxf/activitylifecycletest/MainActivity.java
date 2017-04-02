8ad53b38ff892c191d68e4adc7608364c98e3849
jinxiaofeng0822 <jinxf0822@foxmail.com>
2017-04-02 20:35:04 +0800

    活动生命周期

--- app/src/main/java/com/jinxf/activitylifecycletest/MainActivity.java
+++ app/src/main/java/com/jinxf/activitylifecycletest/MainActivity.java
@@ -1,13 +1,86 @@
 package com.jinxf.activitylifecycletest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);//commit github
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
         setContentView(R.layout.activity_main);
+        if (savedInstanceState!=null){
+            String tempData = savedInstanceState.getString("data_key");
+            Log.d(TAG, "onCreate: "+tempData);
+        }
+
+        Button startNButton = (Button) findViewById(R.id.start_normal_activity);
+        Button startDButton = (Button) findViewById(R.id.start_dialog_activity);
+        startNButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Normal_Activity.class);
+                startActivity(intent);
+            }
+        });
+        startDButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,DialogActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    /**
+     * 对临时数据进行保存
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String tempData = "something you just type";
+        outState.putString("data_key",tempData);
     }
 }
